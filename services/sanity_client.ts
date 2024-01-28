8e89fd0b60497bb229c17d01fa0869f6c36e4504
Elbyss <elysiabyss@gmail.com>
2024-01-28 10:59:31 +0900

    주석처리 제거

--- services/sanity_client.ts
+++ services/sanity_client.ts
@@ -7,13 +7,3 @@ export const client = createClient({
   apiVersion: '2024-01-23', // use current date (YYYY-MM-DD) to target the latest API version
   token: process.env.SANITY_SECRET_TOKEN,
 });
-
-// const doc = {
-//   _id: 'my-bike',
-//   _type: 'bike',
-//   name: 'Sanity Tandem Extraordinaire',
-//   seats: 2,
-// };
-// client.createIfNotExists(doc).then((res) => {
-//   console.log('Bike was created (or was already present)');
-// });
