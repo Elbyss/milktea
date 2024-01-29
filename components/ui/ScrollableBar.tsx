013f9f832d4debc07c9a603f0676611138b678d4
Elbyss <elysiabyss@gmail.com>
2024-01-29 13:58:54 +0900

    스크롤 넘어가는 개수 수정

--- components/ui/ScrollableBar.tsx
+++ components/ui/ScrollableBar.tsx
@@ -9,10 +9,12 @@ export default function ScrollableBar({
     desktop: {
       breakpoint: { max: 3000, min: 1024 },
       items: 6,
+      slidesToSlide: 6,
     },
     mobile: {
       breakpoint: { max: 576, min: 0 },
       items: 4,
+      slidesToSlide: 4,
     },
   };
   return (
